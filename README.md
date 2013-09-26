a4845ef79da709e785c007ed0cf128936621b0c9
wiemag <w.magusiak@gmail.com>
2013-09-26 15:59:50 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,61 @@
+Safe storage
+------------
+
+Script safe-store-prep creates \*.tar.xz.gpg files out of the first level subdirectories and files in a chosen directory in order to prepare them to storage in a non-safe environment, especially in a public storage service like google disk, dropbox, ubuntu one, etc.
+
+
+PREMISE/PURPOSE
+
+To make storage in the cloud safer.
+
+
+WHY THIS FORMAT/SOLUTION
+- The size of a directory may be too big. Even compression will not reduce the size enough for easy uploading. As a solution the directory is split into compressed sub-folders and files of the first depth level. The compression type used is '.tar.xz'.
+- The GPG encryption makes it possible to encode the compressed files for safe storage.
+
+
+USAGE
+
+See the Installation section below, or just run
+	safe-storage-prep.sh --help
+	or
+	safe-storage-prep.sh -h
+
+The script creates md5 check sums for the folders and files to be safely stored and compares the sums to the previously created check sums before creating \*.tar.xz.gpg files. The check sums are stored in a file in /$HOME/.config/md5s.
+
+To work faster, the script does not create .tar.xz.gpg files if the check sums for a given folder or file has not changed.
+
+If you want to force the script to create a .tar.xz.gpg file modify 
+	/$HOME/.config/safe-storage-prep/md5s 
+file, or delete it to force a complete re-creation of all gpg's.
+
+Warning! Any \*.tar.xz files that are already in the directory's first level when the script starts may be removed when their core name matches the name of a folder or file that will be prepared for safe storage.
+
+
+INSTALLATION
+
+As a bash script, 'safe-storage-prep.sh' does not need an installation process. However, it is assumed that the path of the directory that is supposed to be safely stored and the GPG recipient whose public key is supposed to be used for encryption will be stored in a configuration file.
+	The configuration file path is hard-coded into the script. It is
+	/$HOME/.config/safe-storage-prep/sf.conf
+Do not create this file. It will be created automatically at the first use. Just follow the usage/help message displayed.
+
+The same configuration file stores the GPG recipient, as well. Again, no configuration file edition is required. The configuration parameters will be stored when the script is called with correct parameters for the first time.
+
+When the configuration file is created, it has to be edited manually in order to modify the default parameters.
+
+
+DEPENDENCIES/CONFLICTS
+
+gnupg 	(Command: gpg)
+tar 	(Command: tar)
+gawk 	(Command: awk)
+sed 	(Command: sed)
+coreutils 	(Command: cut)
+
+No conflicts.
+
+
+FURTHER DEVELOPMENT
+-The first improvement that comes to mind is adding a command line switch to save new parameters in the configuration file. However, I will not work on it in the foreseeable future. Manual config-file modification is not that complicated after all ;).
+- The second thing is optimizing the script.
+
---
+++ safe-storage-prep.sh
@@ -0,0 +1,148 @@
+#!/bin/bash
+# safe-storage-prep.sh prepares files and folders for storage in a non-safe environment
+# e.g. cloud servers like google disk, dorpbox, ubuntu one and the like.
+#
+# It compresses files and subdirestories of a given directory to the tar.xz format.
+# It encrypts them with gpg and creates md5 check sums of the created gpg files.
+# It stores the sums for future references. 
+# Only files/folders whose check sums have been modified are then uploaded/stored again.
+VERSION="0.1"
+CDIR="$HOME/.config/safe-storage" 	# Configuration/check-sums directory
+CONF="${CDIR}/sf.conf" 				# Configuration file
+#LOGF="${CDIR}/sf.log" 				# Log file
+if [[ ! -d "$CDIR" ]]; then
+	mkdir -p "$CDIR"
+	echo "# Diretory to be prepared for safe storage." > "$CONF"
+	echo "SourceDIR=" >> "$CONF"
+	echo "# GPG encryption recepient." >> "$CONF"
+	echo "GPGRecepient=" >> "$CONF"
+#	echo "Created on $(date +%F)" > "$LOGF"
+fi
+# SDIR = Source directpry/The directory to be prepared for safe storage
+SDIR=$(grep ^SourceDIR "$CONF" | cut -d= -f2)
+GPGR=$(grep ^GPGRecepient "$CONF" | cut -d= -f2)
+function usage () {
+	echo -e "\nUsage:"
+	echo -e "\t\e[33;1m${0##*/} [-d directory-path] [-r recepient] | [-h] | [-v]\e[0m"
+	echo -e "Where:\n\t-d - Path to the folder to be safely stored"
+	echo -e "\t-r - GPG encription recepient"
+	echo -e "\t-v - Shows this script version."
+	echo -e "\t-h - This help message"
+}
+function def_source () {
+	if [[ "x${SDIR}" == "x" ]]; then
+		if [[ -d "$1" ]]; then
+			SRC=${1%/} 			# Remove trailing slash if there is one.
+			SRC=${SRC//\//\\/} 	# Prepare for 'sed -i'.
+			sed -i "s/^SourceDIR=/SourceDIR=${SRC}/;" "$CONF"
+		fi
+	fi
+	SDIR=${1%/}
+}
+function def_recepient () {
+	KEYS=$(gpg -k $1 2>/dev/null | grep pub | wc -l) 	# Number of public keys matching.
+	if [[ $KEYS -eq 1 ]]; then
+		if [[ "x${GPGR}" == "x" ]]; then
+			sed -i "s/^GPGRecepient=/GPGRecepient=$1/;" "$CONF"
+		fi
+		GPGR=$1
+	else
+		if [[ $KEYS -eq 0 ]]; then
+			echo "No public GPG key for recepient '$1'."
+			echo "Run 'gpg -k' to check available keys."
+			exit 3
+		else
+			echo "More than one public GPG keys for '$1' available."
+			echo "Run 'gpg -k' or 'gpg -k $1' to check available keys."
+			exit 4
+		fi
+	fi
+}
+function create_gpg () {
+	echo -e "\t\t\e[31mCreating ${1}.tar.xz.gpg\e[0m"
+	tar -cJf "${1}.tar.xz" "$1" 
+	gpg --yes -er $2 "${1}.tar.xz"
+	rm "${1}.tar.xz"
+}
+
+#--- Set options and default parameters --------------------------------
+while getopts  ":d:r:hv" flag
+do
+    case "$flag" in
+		h) usage && exit;;
+		v) echo "Version ${VERSION}"; exit;;
+		d) def_source "$OPTARG";;
+		r) def_recepient "$OPTARG";;
+	esac
+done
+if [[ ! -d "$SDIR" ]]; then
+	if [[ x"$SDIR" = "x" ]]; then
+		usage 
+		echo -e "\nDiretory to be prepared for safe storage is not defined."
+		echo -e "\e[33mUse the \e[33;1m-d\e[0m\e[33m option "
+		echo -e "or edit the '${CONF}' configuration file.\e[0m"
+		exit 1
+	fi
+	echo "Source directory not available or non-existent."
+	exit 2
+fi
+if [[ x"$GPGR" = "x" ]]; then
+	usage 
+	echo -e "\nNo GPG recepient defined."
+	echo -e "\e[33mUse the \e[33;1m-r\e[0m\e[33m option."
+	echo -e "or edit the '${CONF}' configuration file.\e[0m"
+	exit 5
+fi
+
+#--- Begin preparation for safe storage --------------------------------
+
+p=$(pwd)
+cd $SDIR
+echo -e "\n\e[1mWarning!\e[0m The already existent *gpg files will be removed."
+for ((t=15;--t;)) {
+	echo -en "\rContinue? (Y/n) [\e[33m$t\e[0m sek] "
+	read -rsn1 -t1 Q && break
+}
+if [[ "$Q" != "Y" && "$Q" != "y" && "$Q" != "" ]]; then
+	echo -e "\rAbandoned.              "
+	exit
+fi
+echo -e "\r                         \nWorking. This may take a few minutes.\nProcessing..."
+NEWSUMS=/tmp/sfgpg.sums
+echo -n "" > $NEWSUMS
+for f in $(ls -1 | grep -v 'gpg$'); do 		# Exclude *gpg files.
+	echo -e "\t$f"
+	if [[ -d "$f" ]]; then
+		NEWCHK=$(find "$f/" -type f -exec md5sum {} +|awk '{print $1}'|sort|md5sum|awk '{print $1}')
+	else
+		NEWCHK=$(md5sum "$f"|awk '{print $1}')
+	fi
+	#echo DEBUG: NEWCHK=$NEWCHK
+	echo "${NEWCHK} ${f}" >> $NEWSUMS
+	if [[ -e "${CDIR}/md5s" ]]; then
+		# The space in " ${f}$" below is important!
+		OLDCHK=$(grep " ${f}$" "${CDIR}/md5s"|cut -d" " -f1)
+		#echo DEBUG: OLDCHK=$OLDCHK
+		if [ "x${OLDCHK}" = x${NEWCHK} ]; then
+			# The check sum has not changed. No need to crate the gpg file.
+			#echo -e "\t\e[32m${f}\e[0m has not changed"
+			true
+		else
+			if [[ -n $OLDCHK ]]; then
+				# The check sum has changed. Create a gpg file.
+				#echo -e "\t\e[36m${f}\e[0m has changed!"
+				true
+			else
+				#echo -e "\t\e[34m${f}\e[0m is a new file/directory"
+				true
+			fi
+			create_gpg $f $GPGR
+		fi
+	else
+		create_gpg $f $GPGR
+	fi
+done
+cp $NEWSUMS "${CDIR}/md5s"
+rm $NEWSUMS
+echo "Done."
+cd "$p"
